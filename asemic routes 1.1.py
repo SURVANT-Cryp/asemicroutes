37013fbcc11169482dbd4ab4ed09180de6b8f69d
patrick jo quinn <SURVANT-Cryp@users.noreply.github.com>
2023-05-01 23:02:58 -0400

    Add files via upload

--- asemic routes 1.1.py
+++ asemic routes 1.1.py
@@ -1,11 +1,10 @@
-#!/Users/patrickquinn/anaconda3/bin/python/
+#!/usr/bin/python
 # coding: utf-8
 #   The open nature of asemic works allows for meaning to occur across linguistic understanding; an asemic text may be “read” in a similar fashion regardless of the reader’s natural language. Multiple meanings for the same symbolism are another possibility for an asemic work, that is, asemic writing can be polysemantic or have zero meaning, infinite meanings, or its meaning can evolve over time.
 
 #   “De Quincey describes using the stars to guide him home. Having no knowledge of celestial navigation he finds himself in unfamiliar territories, discovering what he believes are streets anonymous to maps, thus reimagining the city in his own eyes.” (http://www.thedoublenegative.co.uk/2014/12/an-introduction-to-psychogeography/)
 
 import string
-import googlemaps
 import random
 import decimal
 import textwrap
@@ -20,22 +19,6 @@ def slowprint(s):
         sys.stdout.flush() # defeat buffering
         time.sleep(random.random() * 0.1)
 
-gmaps = googlemaps.Client(key='AIzaSyAXmnIhG7oE51Pn9XpD9CZOnz12h1Wl8TA')
-
-top_left=gmaps.reverse_geocode((40.9095863,-73.8919719))
-bottom_left=gmaps.reverse_geocode((40.5945483, -74.0584152))
-top_right=gmaps.reverse_geocode((40.7924230, -73.7799160))
-bottom_right=gmaps.reverse_geocode((40.5894739,-73.7537850))
-
-# get random latitude
-rand_lat = float(decimal.Decimal(random.randrange(405945483,409095863))/10000000)
-
-# get random longitude
-rand_long = float(decimal.Decimal(random.randrange(-740584152,-737537850))/10000000)
-
-random_address=gmaps.reverse_geocode((rand_lat,rand_long))
-
-print '\n' + "Suggested Direction/Destination:" + '\n' + (random_address[0]['formatted_address'])
 
 # Just alphanumeric characters
 chars = string.letters + string.digits
